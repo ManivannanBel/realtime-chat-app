f302390eb40351f964e051bb0dcd51e5650d856f
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2020-02-01 12:46:36 +0530

    client chat component

--- real-time-chat-app-client/src/componenets/Join.jsx
+++ real-time-chat-app-client/src/componenets/Join.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 
 function Join() {
+
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
     return (
-        <div>
-            join
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join</h1>
+                <div><input placeholder="" className="joinInput" type="text" onChange={(event) => setName(event.target.value)}/></div>
+                <div><input placeholder="" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.name)}/></div>
+                <Link to={`/chat?name=${name}&room=${room}`} onclick={event => (!name || !room) ? event.preventDefault() : null}>
+                    <button className="button mt20" type="submit">Sign in</button>
+                </Link>
+            </div>
         </div>
     )
 }

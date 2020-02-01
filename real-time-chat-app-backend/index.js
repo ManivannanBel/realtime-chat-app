0ac7400027c083c5575a407ab36a8e4f42ae5964
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2020-02-01 14:47:56 +0530

    emitters and listeners backend

--- real-time-chat-app-backend/index.js
+++ real-time-chat-app-backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, getUser, getUserRoom, removeUser } = require('./users');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -14,17 +16,26 @@ io.on('connection', (socket) => {
     console.log('we have new connection!!!!')
 
     socket.on('join', ({name, room}, callback) => {
-        console.log(name+" "+room);
-
-        const error = true;
+        
+        const {error, user} = addUser({id : socket.id, name, room});
 
         if(error){
-            //callback({error : 'error'});
-        }else{
-
+            return callback(error);
         }
 
+        socket.emit('message', {user:'admin', text: `${user.name}, welcome to the room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', {user : 'admin', text : `${user.name} has joined the chat`})
+
+        socket.join(user.room);
+
+        callback();
     });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit({user : user.name, text : message});
+        callback();
+    })
 
     socket.on('disconnect', () => {
         console.log('disconnected');    
